6631c3b81b6b8228e0d120b7a73d751acf3f40a6
NidhiBorasia103 <153322149+NidhiBorasia103@users.noreply.github.com>
2026-01-17 10:48:15 -0500

    Delete basics_sql.sql

--- basics_sql.sql
+++
@@ -1,5 +0,0 @@
-SELECT *
-From parks_and_recreation.employee_demographics;
-
-SELECT first_name,last_name,birth_date,age, age + 10,gender, birth_date
-From parks_and_recreation.employee_demographics;
